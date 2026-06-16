3c5be63af7a3064fe74c87ca1ed15e5007bc78ce
Phan Hoang Trong Tin <Chouphan1207@gmail.com>
2026-06-16 19:02:26 +0700

    Fix environment Variable Interception

--- src/routes/categoryRoutes.js
+++ src/routes/categoryRoutes.js
@@ -1,30 +1,15 @@
 import express from 'express';
-// import { getCategories, createCategory, editCategory, deleteCategory } from '../controllers/categoryController';
+import {
+  getCategories,
+  createCategory,
+  editCategory,
+  deleteCategory
+} from '../controllers/categoryController.js';
 
 const router = express.Router();
-
-// 1. GET - Fetch all categories (Matches AppUrls.dashboardCategories)
-router.get('/', (req, res) => {
-  res.send('Fetch all categories with tasks left/done');
-});
-
-// 2. POST - Create a new category (Matches AppUrls.createCategory)
-router.post('/create', (req, res) => {
-  // You can access incoming data via req.body (title, icon, color)
-  res.send('Create category');
-});
-
-// 3. PUT - Edit an existing category (Matches `${AppUrls.createCategory}/$id`)
-router.put('/create/:id', (req, res) => {
-  // You can access the specific category ID via req.params.id
-  const categoryId = req.params.id;
-  res.send(`Edit category with ID: ${categoryId}`);
-});
-
-// 4. DELETE - Delete a category (Matches `${AppUrls.createCategory}/$id`)
-router.delete('/create/:id', (req, res) => {
-  const categoryId = req.params.id;
-  res.send(`Delete category with ID: ${categoryId}`);
-});
+router.get('/', getCategories);
+router.post('/', createCategory);
+router.put('/:id', editCategory);
+router.delete('/:id', deleteCategory);
 
 export default router;
